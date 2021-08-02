a74328e199ffcc8a9563e7f21513bdd487cb0691
linox2000 <mllino1@hotmail.com>
2021-08-02 11:50:25 +0100

    start userinput validation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,33 @@
-const addMovieBtn = document.querySelector('header button');
-const addMovieModal = document.getElementById('add-modal');
-const backdrop = document.getElementById('backdrop');
-const cancelBtn = document.querySelector('.btn--passive');
-const userInputs = document.querySelectorAll('input')
+const startAddMovieBtn = document.querySelector("header button");
+const addMovieModal = document.getElementById("add-modal");
+const backdrop = document.getElementById("backdrop");
+const cancelBtn = document.querySelector(".btn--passive");
+const userInputs = document.querySelectorAll("input");
+const addMovieBtn = document.querySelector(".btn--success");
 
-const toggleBackdrop = ()=>{
-    backdrop.classList.toggle('visible')
-}
-
-const toggleMovieModalHandler = ()=>{
-    addMovieModal.classList.toggle('visible');
-    toggleBackdrop()
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
 };
 
-addMovieBtn.addEventListener('click',toggleMovieModalHandler);
-backdrop.addEventListener('click',toggleMovieModalHandler);
-cancelBtn.addEventListener('click',toggleMovieModalHandler)
+const toggleMovieModalHandler = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
 
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+  if (
+    titleValue === "" ||
+    imageUrlValue === "" ||
+    ratingValue === "" ||
+    +ratingValue > 5 ||
+    +ratingValue < 1
+  ) alert('Porfavor introduz valores entre 1 a 5')
+};
+
+startAddMovieBtn.addEventListener("click", toggleMovieModalHandler);
+backdrop.addEventListener("click", toggleMovieModalHandler);
+cancelBtn.addEventListener("click", toggleMovieModalHandler);
+addMovieBtn.addEventListener("click",addMovieHandler);
