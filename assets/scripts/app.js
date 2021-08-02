c7cb2c156c4cc6f9e153b83efefe49c0cd6c0512
linox2000 <mllino1@hotmail.com>
2021-08-02 11:34:41 +0100

    finished the addMovieBtn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,19 @@
-alert('Hello World')
+const addMovieBtn = document.querySelector('header button');
+const addMovieModal = document.getElementById('add-modal');
+const backdrop = document.getElementById('backdrop');
+const cancelBtn = document.querySelector('.btn--passive');
+const userInputs = document.querySelectorAll('input')
+
+const toggleBackdrop = ()=>{
+    backdrop.classList.toggle('visible')
+}
+
+const toggleMovieModalHandler = ()=>{
+    addMovieModal.classList.toggle('visible');
+    toggleBackdrop()
+};
+
+addMovieBtn.addEventListener('click',toggleMovieModalHandler);
+backdrop.addEventListener('click',toggleMovieModalHandler);
+cancelBtn.addEventListener('click',toggleMovieModalHandler)
+
